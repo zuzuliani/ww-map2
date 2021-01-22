e8b3d8228c1324e2475abb885be7f0ed720f378d
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-01-22 14:58:32 +0100

    Fix ww-map

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -73,12 +73,12 @@ export default {
             }
             return true;
         },
-        isGoogleKeyMatch() {
-            if (this.content.googleKey) {
-                return this.content.googleKey.match(/^(AIza[0-9A-Za-z-_]{35})$/);
-            }
-            return false;
-        },
+        // isGoogleKeyMatch() {
+        //     if (this.content.googleKey) {
+        //         return this.content.googleKey.match(/^(AIza[0-9A-Za-z-_]{35})$/);
+        //     }
+        //     return false;
+        // },
     },
     watch: {
         'content.googleKey'() {
