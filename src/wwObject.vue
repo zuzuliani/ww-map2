fe90619802d78ca92b5d84f3d52675828d637bb5
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-01-23 17:18:47 +0100

    fix

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -101,97 +101,97 @@ export default {
         },
     },
     methods: {
-        initMap() {
-            const { lat, lng, zoom, googleKey } = this.content;
-            if (!this.isGoogleKeyMatch) {
-                if (googleKey && googleKey.length) this.wrongKey = true;
-                setTimeout(() => {
-                    this.wrongKey = false;
-                }, 8000);
-                return;
-            }
-            this.wrongKey = false;
-            if (!lat || !lng || !zoom || !googleKey.length) return;
-            if (this.loader) {
-                this.loader.reset();
-            }
-            this.loader = new Loader({
-                apiKey: googleKey,
-                language: wwLib.wwLang.lang,
-            });
-            const mapOptions = {
-                center: {
-                    lat: parseFloat(lat),
-                    lng: parseFloat(lng),
-                },
-                zoom: zoom,
-                styles: stylesConfig[`${this.content.mapStyle}`],
-            };
-            this.loader
-                .load()
-                .then(() => {
-                    this.googleMapInstance = new google.maps.Map(this.$refs.map, mapOptions);
-                })
-                .catch(err => {
-                    wwLib.wwLog.error(err);
-                });
-            if (this.markers && this.markers.length) {
-                this.addMarkers();
-            }
-        },
-        async openMarkersPopup() {
-            try {
-                const result = await addMarkers({
-                    markers: this.markers,
-                });
-                if (result.markers && result.markers.length) {
-                    this.markers = result.markers;
-                    this.addMarkers();
-                }
-            } catch (err) {
-                wwLib.wwLog.error(err);
-            }
-        },
-        addMarkers() {
-            if (this.markerInstances.length > 0) {
-                for (let markerInstance of this.markerInstances) {
-                    markerInstance.setMap(null);
-                    markerInstance = null;
-                }
-                this.markerInstances = [];
-            }
-            if (this.loader) {
-                for (let marker of this.markers) {
-                    this.loader
-                        .load()
-                        .then(() => {
-                            if (!marker.isActive) return;
-                            const latlng = { lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) };
-                            let _marker = new google.maps.Marker({
-                                position: latlng,
-                                map: this.googleMapInstance,
-                                animation: google.maps.Animation.DROP,
-                            });
-                            this.markerInstances.push(_marker);
-                            if (marker.name) {
-                                const infowindow = new google.maps.InfoWindow({
-                                    content: marker.name,
-                                    maxWidth: 200,
-                                });
-                                _marker.addListener('mouseover', function () {
-                                    infowindow.open(this.googleMapInstance, _marker);
-                                });
-                                _marker.addListener('mouseout', function () {
-                                    infowindow.close();
-                                });
-                            }
-                        })
-                        .catch(err => {
-                            wwLib.wwLog.error(err);
-                        });
-                }
-            }
-        },
+        // initMap() {
+        //     const { lat, lng, zoom, googleKey } = this.content;
+        //     if (!this.isGoogleKeyMatch) {
+        //         if (googleKey && googleKey.length) this.wrongKey = true;
+        //         setTimeout(() => {
+        //             this.wrongKey = false;
+        //         }, 8000);
+        //         return;
+        //     }
+        //     this.wrongKey = false;
+        //     if (!lat || !lng || !zoom || !googleKey.length) return;
+        //     if (this.loader) {
+        //         this.loader.reset();
+        //     }
+        //     this.loader = new Loader({
+        //         apiKey: googleKey,
+        //         language: wwLib.wwLang.lang,
+        //     });
+        //     const mapOptions = {
+        //         center: {
+        //             lat: parseFloat(lat),
+        //             lng: parseFloat(lng),
+        //         },
+        //         zoom: zoom,
+        //         styles: stylesConfig[`${this.content.mapStyle}`],
+        //     };
+        //     this.loader
+        //         .load()
+        //         .then(() => {
+        //             this.googleMapInstance = new google.maps.Map(this.$refs.map, mapOptions);
+        //         })
+        //         .catch(err => {
+        //             wwLib.wwLog.error(err);
+        //         });
+        //     if (this.markers && this.markers.length) {
+        //         this.addMarkers();
+        //     }
+        // },
+        // async openMarkersPopup() {
+        //     try {
+        //         const result = await addMarkers({
+        //             markers: this.markers,
+        //         });
+        //         if (result.markers && result.markers.length) {
+        //             this.markers = result.markers;
+        //             this.addMarkers();
+        //         }
+        //     } catch (err) {
+        //         wwLib.wwLog.error(err);
+        //     }
+        // },
+        // addMarkers() {
+        //     if (this.markerInstances.length > 0) {
+        //         for (let markerInstance of this.markerInstances) {
+        //             markerInstance.setMap(null);
+        //             markerInstance = null;
+        //         }
+        //         this.markerInstances = [];
+        //     }
+        //     if (this.loader) {
+        //         for (let marker of this.markers) {
+        //             this.loader
+        //                 .load()
+        //                 .then(() => {
+        //                     if (!marker.isActive) return;
+        //                     const latlng = { lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) };
+        //                     let _marker = new google.maps.Marker({
+        //                         position: latlng,
+        //                         map: this.googleMapInstance,
+        //                         animation: google.maps.Animation.DROP,
+        //                     });
+        //                     this.markerInstances.push(_marker);
+        //                     if (marker.name) {
+        //                         const infowindow = new google.maps.InfoWindow({
+        //                             content: marker.name,
+        //                             maxWidth: 200,
+        //                         });
+        //                         _marker.addListener('mouseover', function () {
+        //                             infowindow.open(this.googleMapInstance, _marker);
+        //                         });
+        //                         _marker.addListener('mouseout', function () {
+        //                             infowindow.close();
+        //                         });
+        //                     }
+        //                 })
+        //                 .catch(err => {
+        //                     wwLib.wwLog.error(err);
+        //                 });
+        //         }
+        //     }
+        // },
     },
     mounted() {
         this.initMap();
