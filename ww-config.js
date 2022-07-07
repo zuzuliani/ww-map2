9dac3115be30ad73cfa6eddedbb744e0ccda5123
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-07-07 15:55:18 +0200

    Make api key bindable

--- ww-config.js
+++ ww-config.js
@@ -124,7 +124,8 @@ export default {
             options: {
                 placeholder: 'Google API key',
             },
-            defaultValue: 'AIzaSyCtcuLlu2_7aP1ZCUI6kAVZY8K4KMJ-BJA',
+            defaultValue: '',
+            bindable: true,
         },
         lat: {
             section: 'settings',
