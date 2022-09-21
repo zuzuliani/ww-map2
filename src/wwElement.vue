26046a437d892fa1c8b97e9c0803afa3b4fb1904
Alexis JC <alexisjc.dev@gmail.com>
2022-09-21 12:47:37 +0200

    feat: add test event

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -242,6 +242,12 @@ export default {
             }
             this.map.fitBounds(mapBounds);
         },
+        /* wwEditor:start */
+        getMarkerTestEvent() {
+            if (!this.markers.length) throw new Error('No markers found');
+            return { marker: this.markers[0] };
+        },
+        /* wwEditor:end */
     },
 };
 </script>
